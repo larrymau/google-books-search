cd4b0aefaffd3d5642e671123b26836a5e7da731
larry-navarrete <larry.navarrete@idoagency.com>
2020-11-17 03:48:24 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css';
+
+import GoogleBooksSearch from "./googleBooksSearch";
+
+function App() {
+  return (
+    <GoogleBooksSearch></GoogleBooksSearch>
+  )
+}
+
+export default App;
